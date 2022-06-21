10f80fa82d6b369d08b2e37b898fea1e388f9ab9
Svetlana Urasinova <svetlana.urasinova@ottonova.de>
2022-06-21 19:16:20 +0200

    Remove unused imports

--- src/app/inactive-users/inactive-users.component.ts
+++ src/app/inactive-users/inactive-users.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { UsersService } from "../shared/services/users.service";
 
 @Component({
