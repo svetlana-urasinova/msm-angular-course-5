99198a91338f0fdb3ec2e8f9bd003c4059e08345
Svetlana Urasinova <svetlana.urasinova@ottonova.de>
2022-06-21 19:25:22 +0200

    Fix counter

--- src/app/shared/services/count.service.ts
+++ src/app/shared/services/count.service.ts
@@ -1,14 +1,18 @@
 export class CountService {
-  settingToActiveCounter: number;
-  settingToInactiveCounter: number;
+  settingToActiveCounter = 0;
+  settingToInactiveCounter = 0;
 
   countSettingToActive(name: string): void {
     this.settingToActiveCounter++;
-    console.log(`Set user ${name} to active users.`);
+    console.log(
+      `Set user ${name} to active users (total ${this.settingToActiveCounter} times)`
+    );
   }
 
   countSettingToInactive(name: string): void {
     this.settingToInactiveCounter++;
-    console.log(`Set user ${name} to inactive users.`);
+    console.log(
+      `Set user ${name} to inactive users (total ${this.settingToInactiveCounter} times)`
+    );
   }
 }
